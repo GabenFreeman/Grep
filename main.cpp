39d6248e782d678d5855b5e4485e655db1548d08
Miki <wudzia9@gmail.com>
2023-04-01 12:48:13 +0200

    working on flags

--- main.cpp
+++ main.cpp
@@ -2,16 +2,55 @@
 #include <fstream>
 #include <string>
 #include <filesystem>
+#include <string.h>
+#include <typeinfo>
+#include <unistd.h>
+
 
 namespace fs = std::filesystem;
 
-int files = 0;
-int nfiles = 0;
-int patterns = 0;
+int search(int argc, char** argv) {
+    int files = 0;
+    int nfiles = 0;
+    int patterns = 0;
 
-void search(const std::string& pattern, const fs::path& path) {
+    //set default Current working directory
+    char dir[256];
+    getcwd(dir, 256);
+    fs::path path = dir;
+
+    //set pattern
+    const std::string pattern = argv[1];
+
+
+    for (int i = 2; i < argc; i+=2){
+        //std::cout << argv[i] << std::endl ;
+        //std::cout << i << std::endl ;
+
+        if(!strcmp(argv[i], "-d") || !strcmp(argv[i], "--dir")){
+            path = argv[i+1];
+            std::cout << "-d" <<": " << argv[i+1] <<std::endl;
+        }
+        else if(!strcmp(argv[i], "-l") || !strcmp(argv[i], "--log_file")){
+            std::cout << "-l" <<": " << argv[i+1]<< std::endl;
+        }
+        else if(!strcmp(argv[i], "-r") || !strcmp(argv[i], "--result_file")){
+            std::cout << "-r" <<": " << argv[i+1] <<std::endl;
+        }
+        else if(!strcmp(argv[i], "-t") || !strcmp(argv[i], "--threads")){
+            std::cout << "-t" <<": " << argv[i+1] <<std::endl;
+        }
+        else{
+            std::cerr << "error\n";
+            return 1;
+        }
+
+    }
+
+
+
     if (!fs::is_directory(path)) {
-        return;
+        return 1;
     }
 
     for (const auto& entry : fs::recursive_directory_iterator(path)) {
@@ -28,9 +67,11 @@ void search(const std::string& pattern, const fs::path& path) {
 
         std::string line;
         bool temp = true;
+        int coutnLine = 0;
         while (std::getline(file, line)) {
+            coutnLine++;
             if (line.find(pattern) != std::string::npos) {
-                std::cout << entry.path() << ": " << line << '\n';
+                std::cout << entry.path() << ":" << coutnLine << ": " << line << '\n';
                 patterns++;
                 if (temp){
                     nfiles++;
@@ -40,20 +81,22 @@ void search(const std::string& pattern, const fs::path& path) {
         }
         files++;
     }
-}
 
-int main(int argc, char** argv) {
-    if (argc != 3) {
-        std::cerr << "Usage: " << argv[0] << " <pattern> <directory>\n";
-        return 1;
-    }
-
-    const std::string pattern = argv[1];
-    const fs::path path = argv[2];
-
-    search(pattern, path);
     std::cout << "Searched files: " << files << '\n';
     std::cout << "Files with pattern: " << nfiles << '\n';
     std::cout << "Patterns number: " << patterns << '\n';
+    return 0;
+}
+
+int main(int argc, char** argv) {
+    /*if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <pattern> <directory>\n";
+        return 1;
+    }*/
+
+    //const std::string pattern = argv[1];
+    //const fs::path path = argv[2];
+
+    search(argc, argv);
     return 0;
 }
