6e508a72c9f3974d38e0dbb0822c803b4c7b1e10
Miki <wudzia9@gmail.com>
2023-04-01 14:11:53 +0200

    flags done

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <typeinfo>
 #include <unistd.h>
+#include <chrono>
 
 
 namespace fs = std::filesystem;
@@ -22,6 +23,18 @@ int search(int argc, char** argv) {
     //set pattern
     const std::string pattern = argv[1];
 
+    //set default result file
+    std::string txt=".txt";
+    std::string resiltFileName = argv[0]+txt;
+    resiltFileName.erase(0, 2);
+
+    //set default log file
+    std::string log=".log";
+    std::string logFileName = argv[0]+log;
+    logFileName.erase(0, 2);
+
+    // Start measuring time
+    auto begin = std::chrono::high_resolution_clock::now();
 
     for (int i = 2; i < argc; i+=2){
         //std::cout << argv[i] << std::endl ;
@@ -32,9 +45,11 @@ int search(int argc, char** argv) {
             std::cout << "-d" <<": " << argv[i+1] <<std::endl;
         }
         else if(!strcmp(argv[i], "-l") || !strcmp(argv[i], "--log_file")){
+            logFileName=argv[i+1]+log;
             std::cout << "-l" <<": " << argv[i+1]<< std::endl;
         }
         else if(!strcmp(argv[i], "-r") || !strcmp(argv[i], "--result_file")){
+            resiltFileName=argv[i+1]+txt;
             std::cout << "-r" <<": " << argv[i+1] <<std::endl;
         }
         else if(!strcmp(argv[i], "-t") || !strcmp(argv[i], "--threads")){
@@ -47,7 +62,11 @@ int search(int argc, char** argv) {
 
     }
 
+    //create resiltFile
+    std::ofstream resiltFile(resiltFileName);
 
+    //create logFile
+    std::ofstream logFile(logFileName);
 
     if (!fs::is_directory(path)) {
         return 1;
@@ -71,7 +90,7 @@ int search(int argc, char** argv) {
         while (std::getline(file, line)) {
             coutnLine++;
             if (line.find(pattern) != std::string::npos) {
-                std::cout << entry.path() << ":" << coutnLine << ": " << line << '\n';
+                resiltFile << entry.path() << ":" << coutnLine << ": " << line << '\n';
                 patterns++;
                 if (temp){
                     nfiles++;
@@ -82,17 +101,26 @@ int search(int argc, char** argv) {
         files++;
     }
 
+    // Stop measuring time and calculate the elapsed time
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+
     std::cout << "Searched files: " << files << '\n';
     std::cout << "Files with pattern: " << nfiles << '\n';
     std::cout << "Patterns number: " << patterns << '\n';
+    std::cout << "Result file: " << resiltFileName << '\n';
+    std::cout << "Log file: " << logFileName << '\n';
+    printf("Time measured: %.6f seconds.\n", elapsed.count() * 1e-9);
+    resiltFile.close();
+    logFile.close();
     return 0;
 }
 
 int main(int argc, char** argv) {
-    /*if (argc != 3) {
+    if (argc < 2) {
         std::cerr << "Usage: " << argv[0] << " <pattern> <directory>\n";
         return 1;
-    }*/
+    }
 
     //const std::string pattern = argv[1];
     //const fs::path path = argv[2];
