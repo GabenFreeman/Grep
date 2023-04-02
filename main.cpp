8c4dca94fb6d217c58c250ddc06d0677b0461458
Miki <wudzia9@gmail.com>
2023-04-02 13:06:26 +0200

    Done without threads

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@
 
 namespace fs = std::filesystem;
 
+
 int search(int argc, char** argv) {
     int files = 0;
     int nfiles = 0;
@@ -36,30 +37,24 @@ int search(int argc, char** argv) {
     // Start measuring time
     auto begin = std::chrono::high_resolution_clock::now();
 
+	//check flags
     for (int i = 2; i < argc; i+=2){
-        //std::cout << argv[i] << std::endl ;
-        //std::cout << i << std::endl ;
-
         if(!strcmp(argv[i], "-d") || !strcmp(argv[i], "--dir")){
             path = argv[i+1];
-            std::cout << "-d" <<": " << argv[i+1] <<std::endl;
         }
         else if(!strcmp(argv[i], "-l") || !strcmp(argv[i], "--log_file")){
             logFileName=argv[i+1]+log;
-            std::cout << "-l" <<": " << argv[i+1]<< std::endl;
         }
         else if(!strcmp(argv[i], "-r") || !strcmp(argv[i], "--result_file")){
             resiltFileName=argv[i+1]+txt;
-            std::cout << "-r" <<": " << argv[i+1] <<std::endl;
         }
         else if(!strcmp(argv[i], "-t") || !strcmp(argv[i], "--threads")){
-            std::cout << "-t" <<": " << argv[i+1] <<std::endl;
+            //not in this program :(
         }
         else{
-            std::cerr << "error\n";
+            std::cerr << "Wrong flag.\n";
             return 1;
         }
-
     }
 
     //create resiltFile
@@ -68,12 +63,12 @@ int search(int argc, char** argv) {
     //create logFile
     std::ofstream logFile(logFileName);
 
+	//search for pattern
     if (!fs::is_directory(path)) {
         return 1;
     }
 
     for (const auto& entry : fs::recursive_directory_iterator(path)) {
-
         if (!entry.is_regular_file()) {
             continue;
         }
@@ -111,6 +106,7 @@ int search(int argc, char** argv) {
     std::cout << "Result file: " << resiltFileName << '\n';
     std::cout << "Log file: " << logFileName << '\n';
     printf("Time measured: %.6f seconds.\n", elapsed.count() * 1e-9);
+
     resiltFile.close();
     logFile.close();
     return 0;
@@ -118,12 +114,28 @@ int search(int argc, char** argv) {
 
 int main(int argc, char** argv) {
     if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " <pattern> <directory>\n";
+        std::cerr << "Usage: " << argv[0] << " <pattern> <flags>\n"
+        "The program has one obligatory parameter which is <pattern (a string)>\n"
+        "and can have four optional parameters.\n"
+        "This program searches for a specified pattern in files within a specified\n"
+        "directory and its subdirectories, and outputs the matches to a result file.\n\n"
+        "+----------------------+--------------------------------+--------------------+\n"
+        "|       Parameter      |          Description           |   Default value    |\n"
+        "+----------------------+--------------------------------+--------------------+\n"
+        "| -d or --dir          | start directory where program  |                    |\n"
+        "|                      | needs to look for files        | current directory  |\n"
+        "|                      | (also in subfolders)           |                    |\n"
+        "+----------------------+--------------------------------+--------------------+\n"
+        "| -l or --log_file     |     name of the log file       | <program name>.log |\n"
+        "+----------------------+--------------------------------+--------------------+\n"
+        "| -r or --result_file  |     name of the file where     | <program name>.txt |\n"
+        "|                      |        result is given         |                    |\n"
+        "+----------------------+--------------------------------+--------------------+\n"
+        "| -t or --threads      |  number of threads in the pool |         4          |\n"
+        "|                      |    (not in this program :( )   |                    |\n"
+        "+----------------------+--------------------------------+--------------------+\n";
         return 1;
     }
-
-    //const std::string pattern = argv[1];
-    //const fs::path path = argv[2];
 
     search(argc, argv);
     return 0;
